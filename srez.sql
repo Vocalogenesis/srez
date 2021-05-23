e063f06379f39a7520388c442cd55678e4b0af3c
Vocalogenesis <vocalogenesis2000@gmail.com>
2021-05-23 20:53:07 +0300

    update3+db

--- srez.sql
+++ srez.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 16 2021 г., 02:40
+-- Время создания: Май 23 2021 г., 20:52
 -- Версия сервера: 5.6.47
 -- Версия PHP: 7.3.17
 
@@ -84,7 +84,12 @@ CREATE TABLE `request` (
 INSERT INTO `request` (`id`, `status`, `name`, `before_img`, `after_img`, `why_not`, `category_id`, `created_at`, `created_by`, `updated_by`) VALUES
 (15, 'Новая', 'Ахахахахаха', 'uploads/aP57X5aD14c.jpg', 'uploads/6f340467eab7ce506adb4b83d916ea3a.png', '', 2, '2021-05-16 01:51:44', 100, 100),
 (16, 'Решена', 'Зачем', 'uploads/orange.png', 'uploads/змеяя.png', '', 2, '2021-05-16 01:52:11', 100, 100),
-(17, 'Решена', 'Рыба', 'uploads/sdssgsd.png', 'uploads/teSiUyPzs4o.jpg', '', 2, '2021-05-16 01:52:40', 100, 100);
+(17, 'Решена', 'Рыба', 'uploads/sdssgsd.png', 'uploads/teSiUyPzs4o.jpg', '', 2, '2021-05-16 01:52:40', 100, 100),
+(19, 'Решена', 'Убить', 'uploads/gsdgsdgsdgsdg.png', 'uploads/1511206581-7721.jpg', '', 2, '2021-05-23 20:27:04', 100, 100),
+(20, 'Решена', 'sd', 'uploads/aP57X5aD14c.jpg', 'uploads/15868790746090.png', '', 2, '2021-05-23 20:29:38', 100, 100),
+(21, 'Решена', 'Чел ты........', 'uploads/dude.jpg', 'uploads/sssss.jpg', '', 2, '2021-05-23 20:31:33', 100, 100),
+(22, 'Отклонена', 'Убить', 'uploads/_TTPMWXvdQk.jpg', 'uploads/a3af0688f4056f2f12868199d65315a191e6f2be_full.png', '', 2, '2021-05-23 20:37:49', 100, 100),
+(23, 'Отклонена', 'k', 'uploads/1645dd880499e4885bb29ba2183bf360.jpg', 'uploads/fgf.png', '', 2, '2021-05-23 20:49:31', 100, 100);
 
 --
 -- Индексы сохранённых таблиц
@@ -123,7 +128,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT для таблицы `request`
 --
 ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
